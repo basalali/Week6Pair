d90119dd4a7cc2f0e6bd1ded56c7f394de7ecd26
Nick Hudgins <nickhudgins@gmail.com>
2019-10-29 09:10:22 -0400

    NH Testing progress

--- 09_Capstone/Capstone.Tests/GetAllSpacesTest.cs
+++ 09_Capstone/Capstone.Tests/GetAllSpacesTest.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data.SqlClient;
+using System.Transactions;
+using Capstone.DAL;
+using Capstone.Models;
 
 namespace Capstone.Tests
 {
-    class GetAllSpacesTest
+    [TestClass]
+    public class GetAllSpacesTest
     {
+        private string ConnectionString { get; } = "Data Source=.\\sqlexpress;Initial Catalog=excelsior_venues;Integrated Security=True";
+        int id = 0;
+        private TransactionScope transaction;
+
+       [TestInitialize]
+       public void Setup()
+        {
+            transaction = new TransactionScope();
+
+            using(SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd;
+                connection.Open();
+                cmd = new SqlCommand("SELECT * FROM space", connection);
+                connection.Close();
+
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Dispose();
+        }
+
+        [TestMethod]
+        public void Get_All_Spaces_Test()
+        {
+            SpaceSqlDAL getAllSpaces = new SpaceSqlDAL(ConnectionString);
+
+            List<Space> spaceList = (List<Space>)getAllSpaces.GetSpaceDetails(id);
+
+            Assert.IsNotNull(spaceList);
+
+        }
+
     }
 }
--- 09_Capstone/Capstone.Tests/VenueSqlDAL_Test.cs
+++ 09_Capstone/Capstone.Tests/VenueSqlDAL_Test.cs
@@ -11,8 +11,10 @@ namespace Capstone.Tests
     [TestClass]
     public class VenueSqlDAL_Test
     {
+        //Test will return a list of all venue names
+
         private string ConnectionString { get; } = "Data Source=.\\sqlexpress;Initial Catalog=excelsior_venues;Integrated Security=True";
-        private int venueName = 0;
+        private int venueID = 0;
 
         private TransactionScope transaction;
 
@@ -25,8 +27,15 @@ namespace Capstone.Tests
 
             SqlCommand cmd;
             connection.Open();
-            cmd = new SqlCommand("SELECT COUNT(*) FROM venue", connection);
-            venueName = (int)cmd.ExecuteScalar();
+            cmd = new SqlCommand("SELECT name FROM venue", connection);
+            
+            cmd = new SqlCommand("SELECT * FROM venue " +
+            "JOIN city ON city.id = venue.city_id " +
+            "JOIN state ON state.abbreviation = city.state_abbreviation " +
+            "JOIN category_venue ON category_venue.venue_id = venue.id " +
+            "JOIN category ON category.id = category_venue.category_id " +
+            "WHERE venue_id = @venue_id ", connection);
+            venueID = (int)cmd.ExecuteScalar();
             connection.Close();
 
         }
@@ -39,7 +48,7 @@ namespace Capstone.Tests
         }
 
         [TestMethod]
-        public void GetVenueInfo()
+        public void Get_Venue_Name()
         {
             //arrange
             VenueSqlDAL venueSql = new VenueSqlDAL(ConnectionString);
@@ -47,7 +56,16 @@ namespace Capstone.Tests
 
             //assert
             Assert.IsNotNull(venuename);
-            Assert.AreEqual(venueName, venuename.Count);
+        }
+
+        [TestMethod]
+        public void Get_Venue_Details()
+        {
+            VenueSqlDAL venueSql = new VenueSqlDAL(ConnectionString);
+            //List<Venue> venueDetails = (List<Venue>)venueSql.GetVenueDetails();
+
+            
+            //Assert.IsNotNull(venueDetails);
         }
 
     }
